4c84e05aed08537f62af5be963ec3d7a2eb6e84c
mehdi zoghinia <55335664+mehdizoghinia@users.noreply.github.com>
2023-02-15 13:27:43 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -4,11 +4,16 @@ SELECT
     cus.city,
     cus.state,
     ord.order_date,
+    -- sales volume and total revenue generated which comes from sales.order_items with the key order_id
     SUM(ite.quantity) as 'Total units',
     SUM(ite.quantity * ite.list_price) as 'Revenue' ,
+    -- name of product that was purchased which comes from production.products with the key product_id
     pro.product_name,
+    -- category of the products that were purchased which comes from production.categories with the key category_id
     cat.category_name,
+    -- store at which the puchases took place which comes from sales.stores with the key store_id
     sto.store_name,
+    -- sale reps who made the sales which comes from sales.staff with the key staff_id
     CONCAT(sta.first_name , ' ', sta.last_name) as 'staff rep'
 
     FROM sales.orders ord 
@@ -25,6 +30,7 @@ SELECT
     JOIN sales.staffs sta
     ON ord.staff_id = sta.staff_id
 
+-- We need to use groupby inorder to use SUM
 GROUP BY
     ord.order_id,
     CONCAT(cus.first_name , ' ', cus.last_name),
